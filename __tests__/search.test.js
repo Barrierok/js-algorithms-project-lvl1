46fd7fbda70fa6777ac35576c6b2b8ffaa5ac27a
Ilya Zubtsov <ilyazubtsov@gmail.com>
2022-06-12 17:34:00 +0300

    add fuzzy search

--- __tests__/search.test.js
+++ __tests__/search.test.js
@@ -4,8 +4,8 @@ import buildSearchEngine from '..';
 let docs;
 
 beforeAll(() => {
-  const doc1 = { id: 'doc1', text: "I can't shoot straight unless I've had a pint!" };
-  const doc2 = { id: 'doc2', text: "Don't shoot shoot shoot that thing at me." };
+  const doc1 = { id: 'doc1', text: "I can't shoot shoot straight unless I've had a pint!" };
+  const doc2 = { id: 'doc2', text: "Don't shoot that thing at me." };
   const doc3 = { id: 'doc3', text: "I'm your shooter." };
 
   docs = [doc1, doc2, doc3];
@@ -23,6 +23,14 @@ test('clear search', () => {
 test('relevancy', () => {
   const searchEngine = buildSearchEngine(docs);
   const result = searchEngine.search('shoot');
+
+  expect(result[0]).toBe('doc1');
+  expect(result[1]).toBe('doc2');
+});
+
+test('fuzzy search', () => {
+  const searchEngine = buildSearchEngine(docs);
+  const result = searchEngine.search('shoot at me');
 
   expect(result[0]).toBe('doc2');
   expect(result[1]).toBe('doc1');
--- src/searchEngine.js
+++ src/searchEngine.js
@@ -1,13 +1,34 @@
-const normalizeText = ({ id, text }) => ({ id, terms: text.match(/\w+/g) });
+const normalizeText = (text) => text.match(/\w+/g);
 
 const calculateOccurrences = (terms, term) => terms
   .reduce((acc, value) => (value === term ? acc + 1 : acc), 0);
 
+const search = (docs, words) => docs
+  .map(({ id, text }) => ({ id, terms: normalizeText(text) }))
+  .map(({ id, terms }) => {
+    const countWords = words.reduce((acc, w) => (terms.includes(w) ? acc + 1 : acc), 0);
+    const occurrences = words.reduce((acc, w) => acc + calculateOccurrences(terms, w), 0);
+    return {
+      id,
+      terms,
+      countWords,
+      occurrences,
+    };
+  })
+  .filter((d) => d.occurrences)
+  .sort((a, b) => {
+    if (a.countWords > b.countWords) {
+      return -1;
+    }
+
+    if (a.countWords < b.countWords) {
+      return 1;
+    }
+
+    return b.occurrences - a.occurrences;
+  })
+  .map((d) => d.id);
+
 export default (docs) => ({
-  search: (value) => docs
-    .map(normalizeText)
-    .map(({ id, terms }) => ({ id, terms, occurrences: calculateOccurrences(terms, value) }))
-    .filter((d) => d.occurrences)
-    .sort((a, b) => b.occurrences - a.occurrences)
-    .map((d) => d.id),
+  search: (value) => search(docs, normalizeText(value)),
 });
